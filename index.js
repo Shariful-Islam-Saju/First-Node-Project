e07df6a0f7a4d30dc9ee61557691507b5581684a
Shariful Islam <sajukhan12905@gmail.com>
2024-08-13 08:18:24 +0600

    making the project Ready

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require("http");
-
+const url = require("url");
 const app = {};
 app.config = {
   port: 3000,
@@ -12,7 +12,15 @@ app.server = () => {
 };
 
 app.handleReq = (req, res) => {
-  res.end("<button>Hello World!</button>");
+  const parsedPath = url.parse(req.url, true);
+  const path = parsedPath.pathname;
+  const trimmedPathName = path.replace(/^\/+|\/+$/g, "");
+  const method = req.method.toLowerCase();
+  const queryStiringObj = parsedPath.query;
+  const headerObj = req.headers;
+
+  res.end("Hello Shariful Islam !!! ");
+  
 };
 
 app.server();
